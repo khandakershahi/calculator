23a915975219f934dd909a3a53a7a1b80c48c628
khandakershahi <khandakershahi@gmail.com>
2025-05-23 17:11:50 +0600

    fin-1

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-function appendToDisplay(){
-
+function appendToDisplay(value){
+    let display = document.getElementById("display")
+    display.value = display.value + value;
+    console.log(value);
 }
 
 
 function clearDisplay(){
-
+    let display = document.getElementById("display")
+    display.value = "";
 }
 
 function deleteLast(){
-
+    let display = document.getElementById("display")
+    display.value = display.value.slice(0,-1)
 }
 
 
 
 function calculatorResult(){
-
+    try {
+        let result = eval(document.getElementById("display").value);
+        console.log(result);
+    document.getElementById("display").value = result;
+    } catch (error) {
+        console.log(error);
+    }
 }
